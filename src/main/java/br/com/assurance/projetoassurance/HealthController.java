75bfbcc199032bb5b72b5e4f9bcc896a856f6eac
giulira <giulira@gmail.com>
2026-06-24 23:05:56 -0300

    add classe

--- src/main/java/br/com/assurance/projetoassurance/HealthController.java
+++ src/main/java/br/com/assurance/projetoassurance/HealthController.java
@@ -1,6 +1,5 @@
 package br.com.assurance.projetoassurance;
 
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
